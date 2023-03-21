782711425f4c18387ad219c7fd138a079adc1a7f
GuiBibeau <g.bibeaulaviolette@gmail.com>
2023-03-21 11:15:55 -0400

    add network deployment commands and config

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -1,3 +1,10 @@
 export const config = {
-  contractAddress: "0x485950f7A14140F5561320229CdD7A2B26e39F9E",
+  '0x13881': {
+    name: 'Mumbai',
+    contractAddress: "0x485950f7A14140F5561320229CdD7A2B26e39F9E",
+  },
+  '0x5': {
+    name: 'Goerli',
+    contractAddress: "0x485950f7A14140F5561320229CdD7A2B26e39F9E",
+  }
 };
